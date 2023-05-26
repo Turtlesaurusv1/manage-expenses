da386bf2301fdfbeeb15b4bf922d6c465c42ea1c
ngnshud <ngnshud@gmail.com>
2023-05-26 15:27:57 +0700

    s

--- api/expense-category/category.service.js
+++ api/expense-category/category.service.js
@@ -2,24 +2,6 @@ const { Pool } = require("pg");
 
 async function create(body){
     console.log(body);
-
-    // const result = await Pool.query(
-    //   `INSERT INTO programming_languages 
-    //   (name, released_year, githut_rank, pypl_rank, tiobe_rank) 
-    //   VALUES 
-    //   (?, ?, ?, ?, ?)`, 
-    //   [
-    //     programmingLanguage.name, programmingLanguage.released_year,
-    //     programmingLanguage.githut_rank, programmingLanguage.pypl_rank,
-    //     programmingLanguage.tiobe_rank
-    //   ]
-    // );
-  
-    // let message = 'Error in creating programming language';
-  
-    // if (result.affectedRows) {
-    //   message = 'Programming language created successfully';
-    // }
   
     return {body};
   }
